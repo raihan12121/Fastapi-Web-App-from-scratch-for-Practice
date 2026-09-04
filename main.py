84e4d1732e20f79ae96503c6f66fe8f36cd1734e
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2026-09-04 20:52:30 +0600

    Getting started with Rest API

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
-@app.get("/")
-def home():
-    return {"message": "Hello, Worl2dd!"}
-
-
 
 
 posts: list[dict] = [
     {
-
         "id": 1,
-    "author": "Muhammad Raihan Molla",
-    "title": "FastAPI is Awesome",
-    "content": "This Framework is really easy to use and super fast.",
-    "date_posted": "September 4, 2026",
+        "author": "Muhammad Raihan Molla",
+        "title": "FastAPI is Awesome",
+        "content": "This Framework is really easy to use and super fast.",
+        "date_posted": "September 4, 2026",
     },
-    
     {
-        "id": 1,
-    "author": "Muhammad Raihan Molla",
-    "title": "FastAPI is Awesome",
-    "content": "This Framework is really easy to use and super fast.",
-    "date_posted": "September 4, 2026",
+        "id": 2,
+        "author": "Muhammad Raihan",
+        "title": "FastAPI is Awesomes",
+        "content": "This Framework is really easy to use and super fasts.",
+        "date_posted": "September 4, 2026",
     },
-      
-
 ]
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse , include_in_schema=False)
 def home():
-    return {"message": "Hello world!"}
+    return f"<h1>{posts[0]['title']}</h1>"
 
 
 @app.get("/api/posts")
