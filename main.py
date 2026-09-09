b54d1fdd4a327a2f3254ab1854e12b63edb0e23e
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2026-09-09 20:53:49 +0600

    added post features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI, Request 
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -32,9 +32,37 @@ posts: list[dict] = [
 @app.get("/",include_in_schema=False, name="home")
 @app.get("/posts",include_in_schema=False, name="posts")
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html",{"posts": posts, "title": "Home"})
+    return templates.TemplateResponse(
+        request, 
+        "home.html",
+        {"posts": posts, "title": "Home"}
+        )
+
+
+app.get("/posts/{post_id}", include_in_schema=False)
+def post_page(Request: Request, post_id: int):
+    for posts in posts:
+        if post.get("id")== post.id:
+            title = posts["title"][:50]
+            return templates.TemplateResponse(
+                request,
+                "post.html",
+                {"post": post, "title": title},
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
 
 
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+
+
+@app.get("/api/posts/{post_id}")
+def get_posts( post_id: int):
+    for post in posts:
+        if post.get("id")==post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
