1eef860bbee4fde62494484a4b9dd5ca6181feae
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2026-09-15 06:48:19 +0600

    Completing CRUD - Update and Delete (PUT, PATCH, DELETE)

--- schemas.py
+++ schemas.py
@@ -18,12 +18,22 @@ class UserResponse(UserBase):
     image_file: str | None = None
 
 
+class UserUpdate(BaseModel):
+    username: str | None = Field(default = None, min_length=1, max_length=50)
+    email: str | None = Field(default = None,min_length=1, max_length=120)
+    image_file: str | None = Field(default = None, min_length=1, max_length=200)
+
+
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
 
 
 class PostCreate(PostBase):
+    user_id: int
+
+
+class PostUpdate(PostBase):
     user_id: int
 
 
